f325c4836c2298aa06bd11a251e81ee357d6d0f8
Bernard Kintzing <bernardkintzing@gmail.com>
2020-02-18 10:20:31 -0700

    Complete sign in and sign up

---
+++ public/assets/js/signup.js
@@ -0,0 +1,26 @@
+function signUp() {
+	errorMessage = document.querySelector("#error-message");
+	name = document.querySelector("#name").value
+	email = document.querySelector("#email").value
+	password = document.querySelector("#password").value
+	confirmPassword = document.querySelector("#confirm-password").value
+
+    console.log("check")
+	if (password == confirmPassword) {
+        console.log("password confirm")
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then(function(result) {
+				result.user.updateProfile({
+					displayName: name
+				});
+				window.location.replace("/home.html");
+			})
+			.catch(function(error) {
+				errorMessage.innerHTML = error.message;
+			});
+	} else {
+        errorMessage.innerHTML = "Passwords do not match"
+    }
+}
