bd767a61494af672508a4a59bcc81f6f9731e874
Bernard Kintzing <bernardkintzing@gmail.com>
2020-02-24 10:43:49 -0700

    Corrected javascript paths

--- public/assets/js/constants.js
+++ public/assets/js/constants.js
@@ -1,1 +1,10 @@
-let ACCOUNT_TYPE_TEACHER = "teacher"
+// Account Types
+let ACCOUNT_TYPE_TEACHER = "teacher";
+let ACCOUNT_TYPE_STUDENT = "student";
+
+// Grades
+let FIRST_GRADE = 1;
+let SECOND_GRADE = 2;
+let THIRD_GRADE = 3;
+let FOURTH_GRADE = 4;
+let FIFTH_GRADE = 5;
