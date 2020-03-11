6f93c889cc38bf2ef22dbf59d63887de383c3851
Bernard Kintzing <bernardkintzing@gmail.com>
2020-03-11 16:52:30 -0600

    Basic game layout

--- public/assets/js/controllers/game.js
+++ public/assets/js/controllers/game.js
@@ -1,3 +1,26 @@
 particlesJS.load('particles-js', '/assets/js/particles/particles.json', function() {
   console.log('particles.js loaded - callback');
 });
+
+// let asteroid = document.getElementById("asteroid");
+function move() {
+  var elem = document.getElementById("asteroid"),
+      speed = 1,
+      currentPos = 0;
+  // Reset the element
+  elem.style.left = 0+"px";
+  elem.style.right = "auto";
+  var motionInterval = setInterval(function() {
+      currentPos += speed;
+      if (currentPos >= 800 && speed > 0) {
+         currentPos = 800;
+         speed = -2 * speed;
+         elem.style.width = parseInt(elem.style.width)*2+"px";
+         elem.style.height = parseInt(elem.style.height)*2+"px";
+      }
+      if (currentPos <= 0 && speed < 0) {
+         clearInterval(motionInterval);
+      }
+      elem.style.left = currentPos+"px";
+  },20);
+}
